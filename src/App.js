65114ad938fb451395ef7887ba37bee23c2a9713
MarkusKhoa <dangkhoa20006@gmail.com>
2021-08-15 05:02:35 +0700

    setup UI

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
-import './App.css';
-import Header from './components/Header';
-import IncomeInfo from './components/IncomeInfo';
-import Balance from './components/Balance';
-import React from 'react';
-import TransactionList from './components/TransactionList';
+import React from 'react'
+import Header from './components/Header'
+import Balance from './components/Balance'
+import "./App.css"
 
 function App() {
   return (
-    <div>
-      <Header className="header"/>
-      <div className="container">
+    <div className="container">
+      <div className="app-wrapper">
+        <Header/>
         <Balance/>
-        <IncomeInfo/>
-        <TransactionList/>
       </div>
     </div>
-  );
+  )
 }
 
 export default App;
