68ea23c0d844010c5cbe409639f318ceb94d4719
MarkusKhoa <dangkhoa20006@gmail.com>
2021-08-15 04:32:35 +0700

    change

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
 import Header from './components/Header';
+import IncomeInfo from './components/IncomeInfo';
+import Balance from './components/Balance';
 import React from 'react';
+import TransactionList from './components/TransactionList';
 
 function App() {
   return (
     <div>
       <Header className="header"/>
+      <div className="container">
+        <Balance/>
+        <IncomeInfo/>
+        <TransactionList/>
+      </div>
     </div>
   );
 }
